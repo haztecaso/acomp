10aaa332450c4d5b4d94b585686ad79b84dff870
Adrián Lattes <adrianlattes@disroot.org>
2022-11-07 09:13:44 +0100

    Actividad 6

---
+++ actividad6.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Adrián Lattes Grassi
+# 30/10/2022
+
+# He incluido las siguientes dos funciones copiadas del módulo modular
+
+def multiplicar_mod(a, b, N):    # 0 <= a,b < N, N >= 1
+   c = a * b
+   c %= N
+   return c
+
+def potencia_mod(a, k, N):             # 0 <= a < N, k >= 0, N >= 2
+    if k == 0:                         # caso base (k = 0)
+        r = 1                          # convencion: 0^0 = 1
+    elif k % 2 == 0:                   # k es par (k > 0)
+        r = potencia_mod(a, k//2, N)
+        r = multiplicar_mod(r, r, N)
+    else:                              # k es impar (k > 0)
+        r = potencia_mod(a, k-1, N)
+        r = multiplicar_mod(a, r, N)
+    return r
+
+def f(n:int, m:int) -> int:
+    """
+    Gracias al teorena de Euler-Fermat se puede hacer la siguiente reducción:
+    f(n,m) = 3^(7^n) mod 10^m = 3^(7^n) mod phi(10^m)) mod 10^m
+
+    Y por otro lado
+    phi(10^m) = phi(2^m*5^m) = phi(2^m)*phi(5^m) = 2^(m-1)*(2-1)*5^(m-1)*(5-1)= 4*10^(m-1)
+    """
+    phi_potencia_10_m = 4*10**(m-1)
+    return potencia_mod(3,potencia_mod(7,n,phi_potencia_10_m), 10**m)
---
+++ test6.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from actividad6 import f
+from timeit import timeit
+
+cases = [
+    (1, 3, 187),
+    (3, 4, 9627),
+    (2022, 10, 4398470083),
+    (
+        5**100,
+        100,
+        3885498536840880390558163388239328561791403020448934831005492927806052986760687686813792112667714187,
+    ),
+]
+
+def test(n,b,expected):
+    assert (
+        f(n, b) == expected
+    ), f"f({n}, {b}) = {f(n,b)} pero debería valer {expected}"
+
+
+if __name__ == "__main__":
+    """
+    Tests
+    """
+    for (n, m, expected) in cases:
+        t = timeit("test(n, m, expected)", globals=globals(), number=1)
+    print("Tests pasados :)")
